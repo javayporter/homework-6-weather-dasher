821aa235d64c723f729d5dbe137d4aa9672f4f40
javayporter <javayporter@gmail.com>
2020-06-18 16:37:20 -0400

    added table to store current weather response, update script.js file to search for city entered on click, updated fonts in style.css

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var apiKey = "ecf05eb98aee0572391da1c4901b75ec";
-var cityEntered = document.getElementById("enter-city").text;
+var dashButton =document.querySelector("#dash-button");
+var cityEntered = document.querySelector("#enter-city");
 
-$("#dash-button").on("click", getWeather());
 
 function getWeather() {
-    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + cityEntered + "&appid=" + apiKey;
+    var city = cityEntered.value.trim();
+    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
 //AJAX Call
 
@@ -15,8 +16,13 @@ $.ajax({
 
     .then(function(response) {
         
+        // Log the city entered
+        console.log(city);
+        //Store city in local storage
+        localStorage.setItem("city", JSON.stringify(city));
+
         // Log the apiCall
-        console.log(apiCall)
+        console.log(apiCall);
 
         //Log response object
         console.log(response);
@@ -25,10 +31,20 @@ $.ajax({
         $(".city").text(response.name + " Weather Details");
         $(".wind-speed").text("Wind Speed: " + response.wind.speed);
         $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".temperature").text("Temperature (K) " + response.main.temp);
+        $(".temperature").text("Temperature (K): " + response.main.temp);
         // $(".uv-index").text("Uv Index: " + response.main.uv)
 
     });
 
    
 };
+
+dashButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    getWeather();
+});
+
+
+localStorage.setItem("cityEntered", JSON.stringify(city));
+
+// $("#dash-button").on("click", getWeather());
